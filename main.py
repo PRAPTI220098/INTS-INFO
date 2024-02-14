3cbaa66fc7bf6f036e89e4746cf592bdca9c212e
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-02-14 11:20:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ try:
 	from telebot import types
 	import flask
 	from user_agent import generate_user_agent
+	from hh import keep_alive
 except:
 	os.system("pip install requests")
 	os.system("pip install telebot")
